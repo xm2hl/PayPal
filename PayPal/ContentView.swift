b03267c5f28d377a34b740cc7c716cc640365d5e
mamdouh al rashidi <mamdouh_m@outlook.sa>
2023-05-17 13:47:42 +0300

    new peg = 0

---
+++ PayPal/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  PayPal
+//
+//  Created by Apple on 17/05/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+         
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PayPal/PayPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  PayPalApp.swift
+//  PayPal
+//
+//  Created by Apple on 17/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct PayPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
